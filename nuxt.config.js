b52221a0c41dc9026fd13dc8e40e268025bcacb8
mrudulks <devmrudulks@gmail.com>
2024-01-10 19:13:33 +0530

    new changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,4 @@
+require("./config")
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -39,7 +40,10 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    proxy: true ,
+  },
+  proxy: {
+    '/api/': { target: process.env.API_URL, pathRewrite: {'^/api/': ''} }
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
@@ -51,5 +55,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+  ssr: true
 }
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,15 @@
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  methods:{
+    async getProducts(){
+      const res = await this.$axios.$get("/api/store/products");
+      console.log("products",res)
+    }
+  },
+  async mounted(){
+    await this.getProducts()
+  }
 }
 </script>
